86abf6a7d14d8a78368fc2344c30fe6c1a45efa0
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-01-29 12:44:39 -0700

    Initial commit

---
+++ PracticeCPPProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PracticeCPPProject
+//
+//  Created by Pearson, William on 1/29/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
